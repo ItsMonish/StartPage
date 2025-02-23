596889543f2027b8aec302b3cb4223cd83327236
Monish Kumar <73230556+ItsMonish@users.noreply.github.com>
2025-02-23 18:49:41 +0530

    [feat]: Extended config parser to parse quicklinks section and refactor

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,19 +9,26 @@ import (
 )
 
 type Configuration struct {
-	Port int                  `yaml:"port"`
-	Rss  map[string][]RSSFeed `yaml:"rss"`
-	Yt   []YTFeed             `yaml:"youtube"`
+	Port  int                       `yaml:"port"`
+	Rss   map[string][]TitleURLItem `yaml:"rss"`
+	Yt    []TitleURLItem            `yaml:"youtube"`
+	Links QuickLinks                `yaml:"quicklinks"`
 }
 
-type RSSFeed struct {
+type TitleURLItem struct {
 	Title string `yaml:"title"`
 	Url   string `yaml:"url"`
 }
 
-type YTFeed struct {
-	Title string `yaml:"title"`
-	Url   string `yaml:"url"`
+type QuickLinks struct {
+	List1Name string         `yaml:"list1Name"`
+	List2Name string         `yaml:"list2Name"`
+	List3Name string         `yaml:"list3Name"`
+	List4Name string         `yaml:"list4Name"`
+	List1     []TitleURLItem `yaml:"list1"`
+	List2     []TitleURLItem `yaml:"list2"`
+	List3     []TitleURLItem `yaml:"list3"`
+	List4     []TitleURLItem `yaml:"list4"`
 }
 
 func GetConfig(logger *log.Logger, configPath string) Configuration {
@@ -54,6 +61,23 @@ func GetConfig(logger *log.Logger, configPath string) Configuration {
 
 	//for _, channel := range config.Yt {
 	//	logger.Println(channel.Title, " - ", channel.Url)
+	//}
+
+	//logger.Println("List 1: ", config.Links.List1Name)
+	//for _, item := range config.Links.List1 {
+	//	logger.Println("Title: ", item.Title, " - ", item.Url)
+	//}
+	//logger.Println("List 2: ", config.Links.List2Name)
+	//for _, item := range config.Links.List2 {
+	//	logger.Println("Title: ", item.Title, " - ", item.Url)
+	//}
+	//logger.Println("List 3: ", config.Links.List3Name)
+	//for _, item := range config.Links.List3 {
+	//	logger.Println("Title: ", item.Title, " - ", item.Url)
+	//}
+	//logger.Println("List 4: ", config.Links.List4Name)
+	//for _, item := range config.Links.List4 {
+	//	logger.Println("Title: ", item.Title, " - ", item.Url)
 	//}
 
 	return config
